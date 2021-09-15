0054af062231bfb8a6feb5c3d97991861a7ae800
Gleb1908 <gleb1908@list.ru>
2021-09-15 18:37:06 +0300

    added class word, where the values of object variables are stored

---
+++ src/main/java/SortingWordsTask/Word.java
@@ -0,0 +1,23 @@
+package SortingWordsTask;
+
+public class Word {
+
+    private String word;
+    private int amount;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
