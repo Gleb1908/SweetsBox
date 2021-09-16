9ae575ff4a2429fbdc9a139d1bde94c47c26718d
Gleb1908 <gleb1908@list.ru>
2021-09-16 11:58:29 +0300

    added Main

---
+++ src/main/java/SortingWordsTask/Main.java
@@ -0,0 +1,13 @@
+package SortingWordsTask;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException  {
+
+        WordsArray wordsArray = new WordsArray(){};
+        wordsArray.fileToArray("src/main/java/SortingWordsTask/Words.txt");
+        wordsArray.mostCommonWords();
+
+    }
+}
