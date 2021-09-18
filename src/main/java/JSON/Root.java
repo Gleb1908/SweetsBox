402b95cf34e2195764ea977dee712f801cfdb65a
Gleb1908 <gleb1908@list.ru>
2021-09-19 00:15:36 +0300

    added Root.java

---
+++ src/main/java/JSON/Root.java
@@ -0,0 +1,15 @@
+package JSON;
+
+import java.util.List;
+
+public class Root {
+
+    private List<Companies> companies;
+
+    public List<Companies> getCompanies() {return companies;}
+
+    public void setCompanies(List<Companies> companies) {
+        this.companies = companies;
+    }
+
+}
