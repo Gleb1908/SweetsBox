be46b0e54223eebd0d833b552f370a0182ab4afa
Gleb1908 <gleb1908@list.ru>
2021-09-19 12:18:47 +0300

    added JSON annotations and constructors

--- src/main/java/JSON/Currency.java
+++ src/main/java/JSON/Currency.java
@@ -1,10 +1,28 @@
 package JSON;
 
-public class Currency {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-    private String rub;
-    private String usd;
-    private String eu;
+import java.io.IOException;
 
+public enum Currency {
+    EU, RUB, USD;
 
+    @JsonValue
+    public String toValue() {
+        switch (this) {
+            case EU: return "EU";
+            case RUB: return "RUB";
+            case USD: return "USD";
+        }
+        return null;
+    }
+
+    @JsonCreator
+    public static Currency forValue(String value) throws IOException {
+        if (value.equals("EU")) return EU;
+        if (value.equals("RUB")) return RUB;
+        if (value.equals("USD")) return USD;
+        throw new IOException("Cannot deserialize Currency");
+    }
 }
