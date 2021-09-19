a40857b15db46b35df2455e9fb8045680d8f90dc
Gleb1908 <gleb1908@list.ru>
2021-09-19 12:19:57 +0300

    added JSON annotations

--- src/main/java/JSON/Root.java
+++ src/main/java/JSON/Root.java
@@ -1,13 +1,17 @@
 package JSON;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class Root {
 
     private List<Companies> companies;
 
+    @JsonProperty("companies")
     public List<Companies> getCompanies() {return companies;}
 
+    @JsonProperty("companies")
     public void setCompanies(List<Companies> companies) {
         this.companies = companies;
     }
