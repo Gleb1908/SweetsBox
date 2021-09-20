b93bdb87808015e914000778423a314f55e693f4
Gleb1908 <gleb1908@list.ru>
2021-09-21 02:16:36 +0300

    some changes

--- src/main/java/JSON/Main.java
+++ src/main/java/JSON/Main.java
@@ -1,13 +1,17 @@
 package JSON;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
 
+        ObjectMapper mapper = new ObjectMapper();
+        Root root = mapper.readValue(new File("src/main/resources/Companies.json"), Root.class);
 
 
     }
